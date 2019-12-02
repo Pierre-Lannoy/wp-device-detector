c1ef709305591851583fa069328758ea1e73c665
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-12-02 19:31:20 +0100

    Adding Spyc & UDD libraries.

--- admin/partials/device-detector-admin-settings-main.php
+++ admin/partials/device-detector-admin-settings-main.php
@@ -11,24 +11,46 @@
 
 // phpcs:ignore
 $active_tab = ( isset( $_GET['tab'] ) ? $_GET['tab'] : 'misc' );
-$url1       = esc_url(
-	add_query_arg(
-		[
-			'page' => 'podd-tools',
-		],
-		admin_url( 'tools.php' )
-	)
-);
-$url2       = esc_url(
-	add_query_arg(
-		[
-			'page' => 'podd-viewer',
-		],
-		admin_url( 'tools.php' )
-	)
-);
-$note       = sprintf(__('Note: <a href="%s">management tools</a> and <a href="%s">analytics reports</a> are available via the <strong>tools menu</strong>.', 'device-detector' ), $url1, $url2 );
 
+use UDD\DeviceDetector;
+use UDD\Parser\Device\DeviceParserAbstract;
+
+// OPTIONAL: Set version truncation to none, so full versions will be returned
+// By default only minor versions will be returned (e.g. X.Y)
+// for other options see VERSION_TRUNCATION_* constants in DeviceParserAbstract class
+DeviceParserAbstract::setVersionTruncation(DeviceParserAbstract::VERSION_TRUNCATION_NONE);
+
+$userAgent = $_SERVER['HTTP_USER_AGENT']; // change this to the useragent you want to parse
+
+$dd = new DeviceDetector($userAgent);
+
+// OPTIONAL: Set caching method
+// By default static cache is used, which works best within one php process (memory array caching)
+// To cache across requests use caching in files or memcache
+// $dd->setCache(new Doctrine\Common\Cache\PhpFileCache('./tmp/'));
+
+// OPTIONAL: Set custom yaml parser
+// By default Spyc will be used for parsing yaml files. You can also use another yaml parser.
+// You may need to implement the Yaml Parser facade if you want to use another parser than Spyc or [Symfony](https://github.com/symfony/yaml)
+// $dd->setYamlParser(new DeviceDetector\Yaml\Symfony());
+
+// OPTIONAL: If called, getBot() will only return true if a bot was detected  (speeds up detection a bit)
+// $dd->discardBotInformation();
+
+// OPTIONAL: If called, bot detection will completely be skipped (bots will be detected as regular devices then)
+// $dd->skipBotDetection();
+
+$dd->parse();
+
+if ($dd->isBot()) {
+	// handle bots,spiders,crawlers,...
+	$text = 'bot / ' . $dd->getBot();
+} else {
+	$text = $dd->getDevice() . ' / ' . $dd->getDeviceName() . ' / ' . $dd->getClient() . ' / ' . $dd->getOs() . ' / ' . $dd->getBrandName() . ' / ' . $dd->getModel() ;
+
+
+
+}
 ?>
 
 <div class="wrap">
@@ -73,5 +95,5 @@ $note       = sprintf(__('Note: <a href="%s">management tools</a> and <a href="%
 	<?php } ?>
 
     <p>&nbsp;</p>
-    <em><?php echo $note;?></em>
+    <em><?php echo $text;?></em>
 </div>
