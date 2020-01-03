99d7778110b53db22ad3af953ee70ef34005a119
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-01-03 07:50:01 +0100

    Discovery for cache introspection endpoint.

--- includes/system/class-cache.php
+++ includes/system/class-cache.php
@@ -103,6 +103,17 @@ class Cache {
 		}
 		self::$apcu_available = function_exists( 'apcu_delete' ) && function_exists( 'apcu_fetch' ) && function_exists( 'apcu_store' );
 		add_action( 'shutdown', [ 'PODeviceDetector\System\Cache', 'log_debug' ], 10, 0 );
+		add_filter( 'perfopsone_icache_introspection', [ 'PODeviceDetector\System\Cache', 'introspection' ] );
+	}
+
+	/**
+	 * Get the introspection endpoint.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function introspection( $endpoints ) {
+		$endpoints[ PODD_SLUG ] = [ 'name' => PODD_PRODUCT_NAME, 'version' => PODD_VERSION, 'endpoint' => [ 'PODeviceDetector\System\Cache', 'get_analytics' ] ];
+		return $endpoints;
 	}
 
 	/**
