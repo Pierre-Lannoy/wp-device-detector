541ac8b4f5c17cd65533ddeb14e5cc41a3969f3c
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-02 12:40:57 +0200

    Fixes issues with IIS/Win10.

--- includes/system/class-environment.php
+++ includes/system/class-environment.php
@@ -35,6 +35,7 @@ class Environment {
 	 */
 	public static function init() {
 		$plugin_path         = str_replace( PODD_SLUG . '/includes/system/', PODD_SLUG . '/', plugin_dir_path( __FILE__ ) );
+		$plugin_path         = str_replace( PODD_SLUG . '\includes\system/', PODD_SLUG . '/', $plugin_path );
 		$plugin_url          = str_replace( PODD_SLUG . '/includes/system/', PODD_SLUG . '/', plugin_dir_url( __FILE__ ) );
 		$plugin_relative_url = str_replace( get_site_url() . '/', '', $plugin_url );
 		define( 'PODD_PLUGIN_DIR', $plugin_path );
